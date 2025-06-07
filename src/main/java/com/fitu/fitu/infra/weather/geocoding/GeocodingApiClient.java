e334227ebe9186408dd06c70b65a541b34efdefe
ottozip <ottozipx@gmail.com>
2025-06-08 01:06:09 +0900

    feat(#5): ApliClient try-catch문으로 예외 처리 추가

--- src/main/java/com/fitu/fitu/infra/weather/geocoding/GeocodingApiClient.java
+++ src/main/java/com/fitu/fitu/infra/weather/geocoding/GeocodingApiClient.java
@@ -1,5 +1,6 @@
 package com.fitu.fitu.infra.weather.geocoding;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+@Slf4j
 @Component
 public class GeocodingApiClient {
 
@@ -22,8 +24,16 @@ public class GeocodingApiClient {
         final HttpEntity<Void> headers = getHttpHeaders();
         final String requestUrl = getRequestUrl(query);
 
-        return restTemplate.exchange(requestUrl, HttpMethod.GET, headers, GeocodingResponse.class)
-                .getBody();
+        GeocodingResponse response = null;
+
+        try {
+            response = restTemplate.exchange(requestUrl, HttpMethod.GET, headers, GeocodingResponse.class).getBody();
+        } catch (final Exception e) {
+            log.error("Geocoding API 호출 중 오류가 발생했습니다. 에러 메세지: {}", e.getMessage());
+            return null;
+        }
+
+        return response;
     }
 
     private HttpEntity<Void> getHttpHeaders() {
